73c73bea8b418fb13469dfae48094930944351f4
javier.gonzalez <javier.gonzalez@piksel.com>
2021-05-16 21:09:37 +0200

    add

---
+++ src/constants/constants.js
@@ -0,0 +1,11 @@
+export const REDUX_SET_ALBUMS = "REDUX_SET_ALBUMS";
+export const REDUX_ADD_ALBUMS = "REDUX_ADD_ALBUMS";
+export const REDUX_SET_ARTISTS = "REDUX_SET_ARTISTS";
+export const REDUX_ADD_ARTISTS = "REDUX_ADD_ARTISTS";
+export const REDUX_SET_TRACKS = "REDUX_SET_TRACKS";
+export const REDUX_ADD_TRACKS = "REDUX_ADD_TRACKS";
+export const ENV = {
+  REACT_APP_CLIENT_ID: "23f1ff0185814de9a657b9f02999a73f",
+  REACT_APP_AUTHORIZE_URL: "https://accounts.spotify.com/authorize",
+  REACT_APP_REDIRECT_URL: "https://javigonz.github.io//redirect",
+};
